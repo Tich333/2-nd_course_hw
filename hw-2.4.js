20a3d405040f7e29b1f74cd44742a2d4a653d047
Tich333 <ramenskiy.a84@mail.ru>
2023-09-06 19:33:20 +0300

    5

---
+++ hw-2.4.js
@@ -0,0 +1,66 @@
+// //Задание № 1
+// let a = 1;
+// let b = 2;
+// while(a <= b) {
+//   alert('Привет!');
+//   a++;
+// }
+
+// //Задание № 2
+// let w = 1;
+// while(w <= 5){
+//   console.log(w)
+//   w++
+// };
+
+// //Задание № 3
+// let t = 7;
+// while(t <= 22){
+//   console.log(t)
+//   t++
+// };
+
+// //Задание № 4
+// const obj = {
+//   'Коля': '200',
+//   'Вася': '300',
+//   'Петя': '400'
+// };
+// for (let key in obj) {
+//   // const name = key;
+//   // const salary = obj[key];
+//   console.log(`${key} — зарплата ${obj[key]} долларов.`);
+// }
+
+// //Задание № 5
+// let n = 1000;
+// let num = 0;
+// while (n >= 50) {
+//   n /= 2;
+//   num++;
+// }
+// console.log("Полученное число: " + n);
+// console.log("Количество итераций: " + num);
+
+// //Задание № 6
+// const firstDayOfMonth = 1; 
+// const friday = 2;
+// for (let day = 1; day <= 31; day++) {
+//   if ((day - 1 + firstDayOfMonth) % 7 === friday - 1) {
+//     console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+//   }
+// }
+// /*второй вариань лучше*/
+// for (let friday = 5; friday <= 31; friday += 7) {
+//   console.log(`Сегодня пятница ${friday}-е число. Необходимо подготовить отчет`);
+// } 
+
+
+
+
+
+
+
+
+
+
